474a4f217efc0757ba32f801244969058a74811f
jackkershaw <hello@jackkershaw.net>
2025-06-19 21:55:47 +0100

    set up mongoose and bodyparser middleware

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
@@ -25,10 +29,18 @@ const urlSchema = new mongoose.Schema({
   shortURL: Number,
 });
 
+let url = mongoose.model("Url", urlSchema);
+
 // Your first API endpoint
-app.post("/api/shorturl", function (req, res) {
-  res.json({ original_url: originalURL, short_url: shortURL });
-});
+app.post(
+  "/api/shorturl",
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
+    let originalURL = req.body["url"];
+    let shortURL = 3;
+    res.json({ original_url: originalURL, short_url: shortURL });
+  }
+);
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
